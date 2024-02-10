411fdeb785e6c483c53f0c5f946e9576e0dce43c
unknown <cogitaciones1@gmail.com>
2024-02-09 22:53:19 -0300

    general styles first

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import pulpo from "../../images/pulpo.png";
+import texto from "../../images/texto.png";
 function Home() {
   const [username, setUsername] = useState();
   const navigate = useNavigate();
-
+  const [onCharge, setOnCharge] = useState(false);
   useEffect(() => {
     localStorage.setItem("user-data", JSON.stringify(null));
   }, []);
 
   const searchUser = async (username) => {
     try {
+      setOnCharge(true);
       const request = await fetch(`https://api.github.com/users/${username}`);
       const response = await request.json();
       if (response?.message !== "Not Found") {
         localStorage.setItem("user-data", JSON.stringify(response));
+        setOnCharge(false);
         navigate(`/profile/${username}`);
       } else {
+        setOnCharge(false);
         alert(` the user ${username} don't exist on GitHub`);
       }
     } catch (error) {
@@ -25,14 +29,25 @@ function Home() {
   };
 
   return (
-    <div>
-      <div>
-        <input
-          type="text"
-          placeholder="input your text here... "
-          onInput={(e) => setUsername(e.target.value)}
-        />
-        <button onClick={() => searchUser(username)}>Search</button>
+    <div className=" vh-100 d-flex justify-content-center align-items-center ">
+      <div className="w-100 d-flex flex-column">
+        <div className=" m-auto d-flex flex-column align-items-center">
+          <img src={pulpo} alt="logo"  width={'70%'} className="img-fluid mb-3" />
+          <img src={texto} alt="logo" width={'60%'} className="img-fluid mb-4" />
+        </div>
+        <h1 className="text-center">Search Devs</h1>
+        <div className="d-flex justify-content-center">
+          <input
+            type="text"
+            placeholder="Type the username here... "
+            onInput={(e) => setUsername(e.target.value)}
+            className="input-search fs-6"
+          />
+          <button onClick={() => searchUser(username)} className="btn-search">
+            <i className="fa-solid fa-magnifying-glass" />{" "}
+            {onCharge ? "Searching..." : "Search"}
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -36,84 +36,92 @@ function Profile() {
   }, [user]);
 
   return (
-    <div className="prof-desc-container">
-      <aside className="aside-container ">
-        <div className="p-5 col-md-6">
-          <img
-            src={userData.avatar_url}
-            alt={userData.avatar_html}
-            className="img-fluid w-100"
-          />
-        </div>
+    <div className="prof-desc-container d-lg-flex">
+      <aside className="aside-container col-lg-5 col-xl-3">
+        <div className="d-flex flex-column col-md-6 col-lg-12 p-lg-4">
+          <div className="p-3 p-md-0 m-md-2">
+            <img
+              src={userData.avatar_url}
+              alt={userData.avatar_html}
+              className="img-fluid w-100"
+            />
+          </div>
 
-        <div className="px-3">
-          <h3>{userData?.name}</h3>
-          <h4>@{userData?.login}</h4>
-        </div>
-        <div className="lorem px-3">
-          <p>{userData.bio}</p>
-        </div>
-        <div className="d-flex flex-column m-3 ">
-          <div className="mb-2">
-            <i className="fa-solid fa-people-group pe-2 fs-1  icon" />
-            <span className="mx-3"> {userData?.followers} followers</span>
-          </div>
-          <div className="mb-2">
-            <i className="fa-regular fa-heart pe-2 fs-1 icon" />
-            <span className="mx-3">{userData.following} following</span>
-          </div>
-          <div className="mb-2">
-            <i className="fa-regular fa-star pe-2 fs-1 icon" />
-            <span className="mx-3">{userData?.public_gists} stars</span>
+          <div className="px-3 ">
+            <h3>{userData?.name}</h3>
+            <h4>@{userData?.login}</h4>
           </div>
         </div>
-        <ul className="list-unstyled m-3 mt-4 d-flex flex-column">
-          <li className="mb-2">
-            <i className="fa-regular fa-building fs-1 icon" />
-            <span className="ms-3">
-              {userData?.company !== null
-                ? userData.company
-                : "No company detailed"}
-            </span>
-          </li>
-          <li className="mb-2 ">
-            <i className="fa-regular fa-map fs-1 icon" />
-            <span className="ms-3">
-              {userData?.location !== null
-                ? userData.location
-                : "No Location detailed"}
-            </span>
-          </li>
-          <li className="mb-2">
-            <i className="fa-regular fa-envelope fs-1 icon " />
-            <span className="ms-3">
-              {userData?.email !== null ? userData.email : "No email detailed"}
-            </span>
-          </li>
-          <li className="mb-2">
-            <i className="fa-solid fa-link fs-1 icon" />
-            <span className="ms-3">
-              {userData?.blog !== "" ? userData.blog : "No web page detailed"}
-            </span>
-          </li>
-          <li className="mb-2">
-            <i className="fa-brands fa-twitter fs-1 icon" />
-            <span className="ms-3">
-              {userData?.twitter_username !== null
-                ? userData.twitter_username
-                : "No  Twitter detailed"}
-            </span>
-          </li>
-        </ul>
-        <div className="d-flex justify-content-center mt-5 pb-5">
-          <Link to={"/"} className="btn-back">
-            Back
-          </Link>
-        </div>
+          <div className="d-flex flex-column-reverse flex-lg-column ">
+            <div className="lorem px-3">
+              <p>{userData.bio}</p>
+            </div>
+            <div className="d-flex flex-column m-3  flex-md-row  ">
+              <div className="mb-2 d-md-flex align-items-md-center ">
+                <i className="fa-solid fa-people-group pe-2 fs-1  icon" />
+                <span className="mx-4"> {userData?.followers} followers</span>
+              </div>
+              <div className="mb-2 d-md-flex align-items-md-center">
+                <i className="fa-regular fa-heart pe-2 fs-1 icon" />
+                <span className="mx-4">{userData.following} following</span>
+              </div>
+              <div className="mb-2 d-md-flex align-items-md-center">
+                <i className="fa-regular fa-star pe-2 fs-1 icon" />
+                <span className="mx-4">{userData?.public_gists} stars</span>
+              </div>
+            </div>
+
+            <ul className="list-unstyled m-3 mt-4 d-flex flex-column ">
+              <li className="mb-2">
+                <i className="fa-regular fa-building fs-1 icon" />
+                <span className="ms-3">
+                  {userData?.company !== null
+                    ? userData.company
+                    : "No company detailed"}
+                </span>
+              </li>
+              <li className="mb-2 ">
+                <i className="fa-regular fa-map fs-1 icon" />
+                <span className="ms-3">
+                  {userData?.location !== null
+                    ? userData.location
+                    : "No Location detailed"}
+                </span>
+              </li>
+              <li className="mb-2">
+                <i className="fa-regular fa-envelope fs-1 icon " />
+                <span className="ms-3">
+                  {userData?.email !== null
+                    ? userData.email
+                    : "No email detailed"}
+                </span>
+              </li>
+              <li className="mb-2">
+                <i className="fa-solid fa-link fs-1 icon" />
+                <span className="ms-3">
+                  {userData?.blog !== ""
+                    ? userData.blog
+                    : "No web page detailed"}
+                </span>
+              </li>
+              <li className="mb-2">
+                <i className="fa-brands fa-twitter fs-1 icon" />
+                <span className="ms-3">
+                  {userData?.twitter_username !== null
+                    ? userData.twitter_username
+                    : "No  Twitter detailed"}
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className="d-flex justify-content-center mt-5 pb-5">
+            <Link to={"/"} className="btn-back">
+              Back
+            </Link>
+          </div>
       </aside>
       <section className="repos-container my-4 mx-3">
-
-<h1 className="mt-3 text-center">Repositories</h1>
+        <h1 className="mt-3 text-center repositories">Repositories</h1>
 
         {reposData.map((repo) => {
           return (
@@ -124,9 +132,9 @@ function Profile() {
                 <div className="d-flex align-items-center">
                   <i className="fa-regular fa-star" />
                   <span className="ms-2 ">{repo.stargazers_count} stars</span>
-                <div className="d-flex justify-content-center p-0 m-0">
-                  <span className="span-circle mx-2"/>
-                </div>
+                  <div className="d-flex justify-content-center p-0 m-0">
+                    <span className="span-circle mx-2" />
+                  </div>
                   <span>
                     updated {updatedCalcDays(repo.updated_at)} days ago
                   </span>
